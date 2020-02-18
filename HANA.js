b51d943cb482feec857dcb5844cbb1b5e0fc8426
MuhammadHanan13 <chyper23@outlook.com>
2020-02-18 14:02:04 +0700

    pesan commit

---
+++ HANA.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+
+class hana extends React.Component{
+    render(){
+        return(
+            <View style={{alignSelf:'center'}}>
+                <Text>HANA</Text>
+            </View>
+        )
+    }
+}
+
+export default hana ;
